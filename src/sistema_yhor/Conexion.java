d1b3ace1baeb17e967491c4998834962febd5d9f
yguerrerocacha-tech <yguerrerocacha@gmail.com>
2026-05-04 01:13:05 -0500

    Sincronizando cambios finales en Conexion y archivos del sistema

--- src/sistema_yhor/Conexion.java
+++ src/sistema_yhor/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/db_goodmarket";
     private static final String USER = "root";
     private static final String PASSWORD = "Guerrero240320";
-    
+ // Última actualización: mayo 2026
     public static Connection conectar() {
         Connection con = null;
         try {
